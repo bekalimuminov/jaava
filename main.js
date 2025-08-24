f5a109ba8cea728d4543eb518b062d461347a331
bekalimuminov <bekalimuminov@gmail.com>
2025-08-24 17:48:10 +0500

    javask

---
+++ main.js
@@ -0,0 +1,29 @@
+// 5-chisi
+// let dollor = 12400
+// let usd = prompt("USD")
+// let sum = usd * dollor
+// alert(sum)
+//4-chisi
+// let ism = prompt("ismingiz")
+// let familya = prompt("familyangiz")
+// alert(ism + familya)
+// 1-chisi
+// let yil = 2025
+// let yosh = prompt("yoshni kiriting")
+// console.log(yil - yosh)
+///3-chisi
+// let son = 8
+// let  raqam = 3
+// alert(son + raqam)
+// let son = 8
+// let  raqam = 3
+// alert(son - raqam)
+// let son = 8
+// let  raqam = 3
+// alert(son * raqam)
+// let son = 8
+// let  raqam = 3
+// alert(son / raqam)
+// let son = 8
+// let  raqam = 3
+// alert(son % raqam)
