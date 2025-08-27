b03f267767f0306ee1e59eafa6aa50cc77eb3e1e
bekalimuminov <bekalimuminov@gmail.com>
2025-08-27 13:01:56 +0500

    dbwhbd

--- main.js
+++ main.js
@@ -1,29 +1,56 @@
-// 5-chisi
-// let dollor = 12400
-// let usd = prompt("USD")
-// let sum = usd * dollor
-// alert(sum)
-//4-chisi
-// let ism = prompt("ismingiz")
-// let familya = prompt("familyangiz")
-// alert(ism + familya)
-// 1-chisi
-// let yil = 2025
-// let yosh = prompt("yoshni kiriting")
-// console.log(yil - yosh)
-///3-chisi
-// let son = 8
-// let  raqam = 3
-// alert(son + raqam)
-// let son = 8
-// let  raqam = 3
-// alert(son - raqam)
-// let son = 8
-// let  raqam = 3
-// alert(son * raqam)
-// let son = 8
-// let  raqam = 3
-// alert(son / raqam)
-// let son = 8
-// let  raqam = 3
-// alert(son % raqam)
+let a = 3, b = 8;
+
+for (let i = a; i <= b; i++) {
+    console.log(i);
+}
+
+let narx = 12000;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i} kg = ${i * narx} so'm`);
+}
+
+let narx2 = 12000;
+
+for (let i = 1; i <= 10; i++) {
+    let w = i / 10;
+    console.log(`${w} kg = ${w * narx2} so'm`);
+}
+
+let a1 = 3, b1 = 8;
+let sum = 0;
+
+for (let i = a1; i <= b1; i++) {
+    sum += i;
+}
+console.log("Yig'indi =", sum)
+
+let a2 = 3, b2 = 6;
+let product = 1;
+
+for (let i = a2; i <= b2; i++) {
+    product *= i;
+}
+console.log("Ko'paytma =", product);
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+let matn = "ld22kje33herjbffnb234234fjhf34";
+let raqamlar = "";
+
+for (let i = 0; i < matn.length; i++) {
+    if (!isNaN(matn[i]) && matn[i] !== " ") {
+        raqamlar += matn[i];
+    }
+}
+console.log("Faqat raqamlar:", raqamlar);
