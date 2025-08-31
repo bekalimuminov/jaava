042ff9cd11150d189000b677a1f97d0d664625cb
bekalimuminov <bekalimuminov@gmail.com>
2025-08-31 16:55:03 +0500

    java

--- main.js
+++ main.js
@@ -1,56 +1,85 @@
-// let a = 3, b = 8;
-// let i = a;
-//
-// while (i <= b) {
-//     console.log(i);
-//     i++;
-// }
-//
-// let narx2 = 12000;
-// let j = 1;
-//
-// while (j <= 10) {
-//     let w = j / 10;
-//     console.log(`${w} kg = ${w * narx2} so'm`);
-//     j++;
-// }
-//
-// let a1 = 3, b1 = 8;
-// let sum = 0;
-// let k = a1;
-//
-// while (k <= b1) {
-//     sum += k;
-//     k++;
-// }
-// console.log("Yig'indi =", sum);
-//
-// let a2 = 3, b2 = 6;
-// let product = 1;
-// let m = a2;
-//
-// do {
-//     product *= m;
-//     m++;
-// } while (m <= b2);
-//
-// console.log("Ko'paytma =", product);
-//
-// let n = 1;
-
-// while (n <= 100) {
-//     switch (true) {
-//         case (n % 3 === 0 && n % 5 === 0):
-//             console.log("FizzBuzz");
-//             break;
-//         case (n % 3 === 0):
-//             console.log("Fizz");
-//             break;
-//         case (n % 5 === 0):
-//             console.log("Buzz");
-//             break;
-//         default:
-//             console.log(n);
+// let a = parseInt(prompt(" son kiriting"));
+// function main() {
+//     if (a > 0) {
+//         console.log(a += 1)
 //     }
-//     n++;
 // }
+//
+// main();
+
+// let a = parseInt( prompt("son kirit"));
+// function main (){
+//     if (a > 0) {
+//         console.log(a += 1)
+//     }else{
+//         console.log(a -= 2)
+//     }
+// }
+// main()
+
+// let a = parseInt(prompt("son kirit"));
+// function main () {
+//     if (a > 0){
+//         console.log(a += 1)
+//     }else if (a < 0){
+//         console.log(a -= 2)
+//     }else{
+//         console.log(a = 10)
+//     }
+// }
+// main ()
+
+// let a = parseInt(prompt("1-sonni kiriting:"));
+// let b = parseInt(prompt("2-sonni kiriting:"));
+// let c = parseInt(prompt("3-sonni kiriting:"));
+// let musbat = 0;
+// function main() {
+//     if (a > 0) musbat++;
+//     if (b > 0) musbat++;
+//     if (c > 0) musbat++;
+//     console.log(musbat)
+// }
+//
+// main();
+
+// let a = parseInt(prompt("1-sonni kiriting:"));
+// let b = parseInt(prompt("2-sonni kiriting:"));
+// let c = parseInt(prompt("3-sonni kiriting:"));
+// let musbat = 0;
+// let manfiy = 0;
+// function main() {
+//     if (a > 0) musbat++; else if (a < 0) manfiy++;
+//     if (b > 0) musbat++; else if (b < 0) manfiy++;
+//     if (c > 0) musbat++; else if (c < 0) manfiy++;
+//     console.log("Musbat sonlar " + musbat )
+//     console.log( "Manfiy sonlar " + manfiy)
+// }
+// main()
+
+// let a = parseInt(prompt("1-sonni kiriting:"));
+// let b = parseInt(prompt("2-sonni kiriting:"));
+// function main() {
+//     if (a > b) {
+//         console.log("Katta son " + a)
+//     }
+//     if (b > a) {
+//         console.log("Katta son " + b)
+//     }
+// }
+// main();
+
+
+// let a = parseInt(prompt("1-sonni kiriting:"));
+// let b = parseInt(prompt("2-sonni kiriting:"));
+// function main() {
+//     if (b < a) {
+//         alert("Katta: " + a);
+//         alert("Kichik: " + b);
+//     }
+//     if (a < b) {
+//         alert("Katta: " + b)
+//         alert("Kichik: " + a);
+//     }
+//
+// }
+// main();
