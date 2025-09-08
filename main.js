96ca61eec030dc87a97971e7fc99f054a24ec218
bekalimuminov <bekalimuminov@gmail.com>
2025-09-08 15:55:21 +0500

    java

--- main.js
+++ main.js
@@ -1,85 +1,43 @@
-// let a = parseInt(prompt(" son kiriting"));
-// function main() {
-//     if (a > 0) {
-//         console.log(a += 1)
-//     }
+//     return Math.floor(n / 10) + (n % 10);
 // }
+// console.log(integer7(57)); // 12
 //
-// main();
-
-// let a = parseInt( prompt("son kirit"));
-// function main (){
-//     if (a > 0) {
-//         console.log(a += 1)
-//     }else{
-//         console.log(a -= 2)
-//     }
+// function integer8(n) {
+//     return (n % 10) * 10 + Math.floor(n / 10);
 // }
-// main()
-
-// let a = parseInt(prompt("son kirit"));
-// function main () {
-//     if (a > 0){
-//         console.log(a += 1)
-//     }else if (a < 0){
-//         console.log(a -= 2)
-//     }else{
-//         console.log(a = 10)
-//     }
-// }
-// main ()
-
-// let a = parseInt(prompt("1-sonni kiriting:"));
-// let b = parseInt(prompt("2-sonni kiriting:"));
-// let c = parseInt(prompt("3-sonni kiriting:"));
-// let musbat = 0;
-// function main() {
-//     if (a > 0) musbat++;
-//     if (b > 0) musbat++;
-//     if (c > 0) musbat++;
-//     console.log(musbat)
-// }
+// console.log(integer8(57)); // 75
 //
-// main();
-
-// let a = parseInt(prompt("1-sonni kiriting:"));
-// let b = parseInt(prompt("2-sonni kiriting:"));
-// let c = parseInt(prompt("3-sonni kiriting:"));
-// let musbat = 0;
-// let manfiy = 0;
-// function main() {
-//     if (a > 0) musbat++; else if (a < 0) manfiy++;
-//     if (b > 0) musbat++; else if (b < 0) manfiy++;
-//     if (c > 0) musbat++; else if (c < 0) manfiy++;
-//     console.log("Musbat sonlar " + musbat )
-//     console.log( "Manfiy sonlar " + manfiy)
+// function integer9(n) {
+//     return Math.floor(n / 100);
 // }
-// main()
-
-// let a = parseInt(prompt("1-sonni kiriting:"));
-// let b = parseInt(prompt("2-sonni kiriting:"));
-// function main() {
-//     if (a > b) {
-//         console.log("Katta son " + a)
-//     }
-//     if (b > a) {
-//         console.log("Katta son " + b)
-//     }
-// }
-// main();
-
-
-// let a = parseInt(prompt("1-sonni kiriting:"));
-// let b = parseInt(prompt("2-sonni kiriting:"));
-// function main() {
-//     if (b < a) {
-//         alert("Katta: " + a);
-//         alert("Kichik: " + b);
-//     }
-//     if (a < b) {
-//         alert("Katta: " + b)
-//         alert("Kichik: " + a);
-//     }
+// console.log(integer9(345)); // 3
 //
+// function integer10(n) {
+//     return { units: n % 10, tens: Math.floor((n % 100) / 10) };
 // }
-// main();
+// console.log(integer10(345)); // {units:5, tens:4}
+//
+// function integer11(n) {
+//     return Math.floor(n / 100) + Math.floor((n % 100) / 10) + (n % 10);
+// }
+// console.log(integer11(345)); // 12
+//
+// function integer12(n) {
+//     return (n % 10) * 100 + Math.floor((n % 100) / 10) * 10 + Math.floor(n / 100);
+// }
+// console.log(integer12(345)); // 543
+//
+// function integer13(n) {
+//     return (n % 100) * 10 + Math.floor(n / 100);
+// }
+// console.log(integer13(345)); // 453
+//
+// function integer14(n) {
+//     return (n % 10) * 100 + Math.floor(n / 10);
+// }
+// console.log(integer14(345)); // 534
+//
+// function integer15(n) {
+//     return Math.floor((n % 100) / 10) * 100 + Math.floor(n / 100) * 10 + (n % 10);
+// }
+// console.log(integer15(123)); // 213
