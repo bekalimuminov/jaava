ce61d4595006f051688150ec31626b6e2638ef02
bekalimuminov <bekalimuminov@gmail.com>
2025-09-14 23:06:08 +0500

    javask

--- main.js
+++ main.js
@@ -1,22 +1,136 @@
-// let L = prompt("sonkirit")
-// let meters = Math.floor(L / 100);
-// console.log("To‘liq metrlar soni:", meters);
-// let M = parseInt(prompt("Og‘irlikni kilogrammda kiriting:"));
-// let tons = Math.floor(M / 1000);
-// console.log("To‘liq tonnalar soni:", tons);
-// let bytes = parseInt(prompt("Fayl hajmini baytlarda kiriting:"));
-// let kilobytes = Math.floor(bytes / 1024);
-// console.log("To‘liq kilobaytlar:", kilobytes);
-// let A = parseInt(prompt("A ni kiriting (A > B):"));
-// let B = parseInt(prompt("B ni kiriting (B < A):"));
-// let count = Math.floor(A / B);
-// console.log("B kesma A kesmada", count, "marta joylashgan");
-// let num = parseInt(prompt("Ikki xonali son kiriting:"));
-// let tens = Math.floor(num / 10);
-// let ones = num % 10;
-// console.log("O‘nliklar:", tens, "Birlar:", ones);
-// let num = parseInt(prompt("Ikki xonali son kiriting:"));
-// let a = Math.floor(num / 10);
-// let b = num % 10;
-// let reversed = b * 10 + a;
-// console.log("Almashtirilgan son:", reversed);
+// let alltypes = ["kitoblar"]
+// alltypes.push("yangi kitob")
+// console.log(alltypes)
+//
+
+// let massiv = []
+// massiv.push(5,21,8551,14,10)
+// console.log(massiv)
+
+// let massiv = ["Bekali" , "Umid", "Muhamadali"]
+// massiv.unshift("Ogabek")
+// console.log(massiv)
+
+// let massiv =["olma","banan","uzum","qulupnay"]
+// massiv.pop()
+// console.log(massiv)
+
+// let massiv = [10,20,22,25,26,84]
+// massiv.pop()
+// massiv.pop()
+// console.log(massiv)
+
+// let navbat = ["Umid","Bekali","Muhammadali","Aziz"]
+// navbat.shift()
+// console.log(navbat)
+
+// let massiv = [5,54,5,4,3]
+// massiv.shift()
+// massiv.shift()
+// massiv.shift()
+// massiv.shift()
+// massiv.shift()
+// console.log(massiv)
+
+// let massiv = [55,12,25,85,65,465,4]
+// massiv.shift()
+// console.log(massiv[0])
+
+// let massiv = []
+// massiv.unshift(21)
+// massiv.unshift(33)
+// massiv.unshift(78)
+// console.log(massiv)
+
+// let type = ["ish qlish","lugat yodlash"]
+// type.unshift("fronted uyga vazifa qilish")
+// console.log(type)
+//
+// let massiv = ["salom" , "xayr"]
+// let doubleNumber= massiv.forEach((number)=>{
+//     console.log("Salom", "Xayr")
+// })
+
+// let massiv = ["Bekali" , "Ogabek"]
+// let doubleNumber = massiv.forEach((number) => {
+//     console.log("salom   " + massiv[0] + "    salom    " + massiv[1])
+// })
+
+
+// let massiv = [1 ,2 ,55, 62, 31, 8]
+// const double = massiv.filter((number) =>{
+//     return number % 2 === 1
+// })
+//
+// console.log(double)
+
+// let year = [10,21,17,18,63];
+// const double = year.filter((number)=>{
+//     return number > 18
+// })
+//
+// console.log(double);
+
+// let year = [10,211,17,18,63];
+// const double = year.find((number)=>{
+//     return number > 100
+// })
+//
+// console.log(double);
+//
+
+// let year = ["Bekali","Ali","Umid"];
+// const double = year.find((number)=>{
+//     return number === "Ali"
+// })
+//
+// console.log(double);
+//
+
+
+// let year = [20 , 50, 100 ,200 , 101,500];
+// const double = year.find((number)=>{
+//     return number > 100
+// })
+//
+// console.log(double);
+//
+
+// const massiv = ["Booklist" ,"javascript", "html"]
+// console.log(massiv.includes("Booklist"))
+
+
+// const mevalar = ["olma","uzum","banan","gilos","anjir","olcha"]
+// console.log(mevalar.includes("gilos"))
+
+// const son = parseInt(prompt("son kirit"))
+// const massiv = [11,2,21,12,1,154,6655,51]
+// console.log(massiv.includes(son))
+
+// const massiv = [1,2,3,4,5]
+// console.log(massiv.length)
+
+// const massiv = []
+// console.log(massiv.length)
+
+//
+// const massiv = ["a", "b", "c", "d", "e", "f"]
+// console.log(massiv.length)
+
+
+// let massiv1 = [1,2]
+// let massiv2 = [3,4]
+//
+// const allarray = massiv1.concat(massiv2)
+// console.log(allarray)
+
+
+// let massiv1 = ["a","b"]
+// let massiv2 = ["c"]
+// let massiv3 = ["d","e"]
+//
+// const allarray = massiv1.concat(massiv2 , massiv3)
+// console.log(allarray)
+
+
+
