230e8c81788af3a64b8d841db0b91f268378cddb
bekalimuminov <bekalimuminov@gmail.com>
2025-09-22 16:11:44 +0500

    hbvddhs

--- main.js
+++ main.js
@@ -1,136 +1,43 @@
-// let alltypes = ["kitoblar"]
-// alltypes.push("yangi kitob")
-// console.log(alltypes)
+// let numbers = [10,20,30,40,50]
 //
+// let DoubleNumber = numbers.slice(1,4)
+// console.log(DoubleNumber)
 
-// let massiv = []
-// massiv.push(5,21,8551,14,10)
-// console.log(massiv)
-
-// let massiv = ["Bekali" , "Umid", "Muhamadali"]
-// massiv.unshift("Ogabek")
-// console.log(massiv)
-
-// let massiv =["olma","banan","uzum","qulupnay"]
-// massiv.pop()
-// console.log(massiv)
-
-// let massiv = [10,20,22,25,26,84]
-// massiv.pop()
-// massiv.pop()
-// console.log(massiv)
-
-// let navbat = ["Umid","Bekali","Muhammadali","Aziz"]
-// navbat.shift()
-// console.log(navbat)
-
-// let massiv = [5,54,5,4,3]
-// massiv.shift()
-// massiv.shift()
-// massiv.shift()
-// massiv.shift()
-// massiv.shift()
-// console.log(massiv)
-
-// let massiv = [55,12,25,85,65,465,4]
-// massiv.shift()
-// console.log(massiv[0])
-
-// let massiv = []
-// massiv.unshift(21)
-// massiv.unshift(33)
-// massiv.unshift(78)
-// console.log(massiv)
-
-// let type = ["ish qlish","lugat yodlash"]
-// type.unshift("fronted uyga vazifa qilish")
-// console.log(type)
+// let latter = ["a","b","c","d","e"];
 //
-// let massiv = ["salom" , "xayr"]
-// let doubleNumber= massiv.forEach((number)=>{
-//     console.log("Salom", "Xayr")
-// })
+// let Doublelatter = latter.slice(3)
+// console.log(Doublelatter);
 
-// let massiv = ["Bekali" , "Ogabek"]
-// let doubleNumber = massiv.forEach((number) => {
-//     console.log("salom   " + massiv[0] + "    salom    " + massiv[1])
-// })
-
-
-// let massiv = [1 ,2 ,55, 62, 31, 8]
-// const double = massiv.filter((number) =>{
-//     return number % 2 === 1
-// })
+// let numbers = [1,2,3,4,5,6]
 //
-// console.log(double)
+// let DoubleNumber = numbers.slice(0 , 4)
+// console.log(DoubleNumber)
 
-// let year = [10,21,17,18,63];
-// const double = year.filter((number)=>{
-//     return number > 18
-// })
+
+// let numbers = [1,2,3,4]
 //
-// console.log(double);
+// numbers.splice(2,1,5)
+// console.log(numbers)
 
-// let year = [10,211,17,18,63];
-// const double = year.find((number)=>{
-//     return number > 100
-// })
+// let latter = ["a","b","c","d"]
 //
-// console.log(double);
+// latter.splice(1,2)
+// console.log(latter)
+
+// let numbers = [10,20,30]
 //
+// numbers.splice(1,1,25)
+// console.log(numbers)
 
-// let year = ["Bekali","Ali","Umid"];
-// const double = year.find((number)=>{
-//     return number === "Ali"
-// })
+
+// let empty = [11,2,5,1,5]
+// empty.fill(0)
+// console.log(empty)
 //
-// console.log(double);
-//
+// let number = [1, 2, 3, 4, 5]
+// number.fill(99,2)
+// console.log(number)
 
-
-// let year = [20 , 50, 100 ,200 , 101,500];
-// const double = year.find((number)=>{
-//     return number > 100
-// })
-//
-// console.log(double);
-//
-
-// const massiv = ["Booklist" ,"javascript", "html"]
-// console.log(massiv.includes("Booklist"))
-
-
-// const mevalar = ["olma","uzum","banan","gilos","anjir","olcha"]
-// console.log(mevalar.includes("gilos"))
-
-// const son = parseInt(prompt("son kirit"))
-// const massiv = [11,2,21,12,1,154,6655,51]
-// console.log(massiv.includes(son))
-
-// const massiv = [1,2,3,4,5]
-// console.log(massiv.length)
-
-// const massiv = []
-// console.log(massiv.length)
-
-//
-// const massiv = ["a", "b", "c", "d", "e", "f"]
-// console.log(massiv.length)
-
-
-// let massiv1 = [1,2]
-// let massiv2 = [3,4]
-//
-// const allarray = massiv1.concat(massiv2)
-// console.log(allarray)
-
-
-// let massiv1 = ["a","b"]
-// let massiv2 = ["c"]
-// let massiv3 = ["d","e"]
-//
-// const allarray = massiv1.concat(massiv2 , massiv3)
-// console.log(allarray)
-
-
-
+// let index = [4,5,6,7]
+// index.indexOf(6)
+// console.log(index)
