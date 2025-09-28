b02604bc281c5213ece6a9d7d6d7436e582bb24d
bekalimuminov <bekalimuminov@gmail.com>
2025-09-28 20:39:34 +0500

    javask

--- main.js
+++ main.js
@@ -1,43 +1,15 @@
-// let numbers = [10,20,30,40,50]
+// const title = document.getElementById("title");
 //
-// let DoubleNumber = numbers.slice(1,4)
-// console.log(DoubleNumber)
-
-// let latter = ["a","b","c","d","e"];
+// const btn = document.getElementById("btn");
 //
-// let Doublelatter = latter.slice(3)
-// console.log(Doublelatter);
+// btn.addEventListener("click",() => {
+//     title.innerHTML = "Assalomu alykum";
+// })
 
-// let numbers = [1,2,3,4,5,6]
+
+// const text = document.querySelector('#text')
+// const btn = document.querySelector('#btn')
 //
-// let DoubleNumber = numbers.slice(0 , 4)
-// console.log(DoubleNumber)
-
-
-// let numbers = [1,2,3,4]
-//
-// numbers.splice(2,1,5)
-// console.log(numbers)
-
-// let latter = ["a","b","c","d"]
-//
-// latter.splice(1,2)
-// console.log(latter)
-
-// let numbers = [10,20,30]
-//
-// numbers.splice(1,1,25)
-// console.log(numbers)
-
-
-// let empty = [11,2,5,1,5]
-// empty.fill(0)
-// console.log(empty)
-//
-// let number = [1, 2, 3, 4, 5]
-// number.fill(99,2)
-// console.log(number)
-
-// let index = [4,5,6,7]
-// index.indexOf(6)
-// console.log(index)
+// btn.addEventListener("click",() => {
+//     document.body.style.color = "red"
+// })
